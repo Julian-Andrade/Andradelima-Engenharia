bf93439d430164ce34815177c7c1524774f4178a
Julian Andrade Silva <72326084+Julian-Andrade@users.noreply.github.com>
2022-04-30 23:13:22 -0300

    "Arquivos da Home Andradelima Engenharia"

---
+++ javascript.js
@@ -0,0 +1,7 @@
+/*Scroll Menu de Navegação*/
+
+window.addEventListener('scroll', function()
+{
+const header = document.querySelector('header');
+header.classList.toggle("sticky", window.scrollY > 0);
+});
